654f7d594bb99241f38d73287898c88082ecf848
Nguyễn  Tuấn Thanh <thanh020801>
2022-04-25 00:26:13 +0700

    start deploy

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ dotenv.config()
 
 module.exports = {
 	port: process.env.PORT || 5000,
-	salt: process.env.SALT,
+	salt: process.env.SALT || 111222333444555666777888999000,
 	DB: {
-		uri: process.env.MONGOO_URI,
+		uri: process.env.MONGOO_URI || 'MONGOO_URI=mongodb+srv://thanh:w1zqVPs5UfWnFJVK@cluster0.ci2u2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 	},
 }
